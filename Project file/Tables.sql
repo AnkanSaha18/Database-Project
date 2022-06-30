f8454d51d83a749ea4903520888918d0048b2cbd
AnkanSaha18 <95054004+AnkanSaha18@users.noreply.github.com>
2022-06-30 17:26:58 +0600

    Check_Up, Ambulance_Book, Medicine_Order, Hospital_Book Tables are added

--- Project file/Tables.sql
+++ Project file/Tables.sql
@@ -1,4 +1,8 @@
 DROP TABLE Blood_Bank_Info;
+DROP TABLE Check_Up;
+DROP TABLE Ambulance_Book;
+DROP TABLE Medicine_Order;
+DROP TABLE Hospital_Book;
 DROP TABLE Services;
 DROP TABLE District;
 DROP TABLE Division;
@@ -7,7 +11,7 @@ DROP TABLE Client;
 CREATE TABLE Division (
     division_name varchar(50),
     PRIMARY KEY(division_name)
-    );
+);
 
 
 CREATE TABLE District(
@@ -15,16 +19,16 @@ CREATE TABLE District(
     division_name varchar(50) NOT NULL,
     CONSTRAINT location PRIMARY KEY(district_name, division_name),
     FOREIGN KEY(division_name) REFERENCES Division (division_name) ON DELETE CASCADE
-    );
+);
 
-Create TABLE Client(
+CREATE TABLE Client(
     id number(6) NOT NULL,
     name varchar(50) NOT NULL,
     gender varchar(10),
     age number(3),
     notes varchar(100),
     PRIMARY KEY (id)
-    );
+);
 
 
 CREATE TABLE Services(
@@ -37,10 +41,61 @@ CREATE TABLE Services(
     division_name varchar(50) NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY(district_name, division_name) REFERENCES District (district_name, division_name) ON DELETE CASCADE
-    );
+);
+
+
+
+
+CREATE TABLE Check_Up(
+    id number(6),
+    client_id number(6) NOT NULL,
+    doctor_id number(6) CHECK (doctor_id BETWEEN 1001 AND 2000),
+    schedule date not null,
+    PRIMARY KEY(id),
+    FOREIGN KEY(client_id) REFERENCES Client(id) ON DELETE CASCADE,
+    FOREIGN KEY(doctor_id) REFERENCES Services(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Ambulance_Book 
+(
+    id number(6),
+    client_id number(6) NOT NULL,
+    ambulance_id number(6) CHECK (ambulance_id BETWEEN 2001 AND 3000),
+    pick_up varchar(50) NOT NULL, 
+    drop_off varchar(50) NOT NULL, 
+    PRIMARY KEY(id),
+    FOREIGN KEY(client_id) REFERENCES Client(id) ON DELETE CASCADE,
+    FOREIGN KEY(ambulance_id) REFERENCES Services(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE Medicine_Order 
+(
+    id number(6),
+    client_id number(6) NOT NULL,
+    pharmacy_id number(6) CHECK (pharmacy_id BETWEEN 3001 AND 4000),
+    medicine_name varchar(50), 
+    quantity number(6),
+    PRIMARY KEY(id),
+    FOREIGN KEY(client_id) REFERENCES Client(id) ON DELETE CASCADE,
+    FOREIGN KEY(pharmacy_id) REFERENCES Services(id) ON DELETE CASCADE
+);
+
+CREATE TABLE Hospital_Book(
+    id number(6),
+    client_id number(6) NOT NULL,
+    hospital_id number(6) CHECK (hospital_id BETWEEN 4001 AND 5000),
+    check_in date NOT NULL,
+    check_out date,
+    PRIMARY KEY(id),
+    FOREIGN KEY(client_id) REFERENCES Client(id) ON DELETE CASCADE,
+    FOREIGN KEY(hospital_id) REFERENCES Services(id) ON DELETE CASCADE
+);
+
 
 CREATE TABLE Blood_Bank_Info(
-    id number(6), 
+    id number(6) CHECK (id BETWEEN 5001 AND 6000), 
     A_P number(6) DEFAULT 0,
     A_N number(6) DEFAULT 0,
     B_P number(6) DEFAULT 0,
@@ -52,5 +107,4 @@ CREATE TABLE Blood_Bank_Info(
     PRIMARY KEY(id),
     FOREIGN KEY(id) REFERENCES Services(id) ON DELETE CASCADE
 );
-
 
--- Project file/Values.sql
+++ Project file/Values.sql
@@ -38,27 +38,63 @@ INSERT INTO Client VALUES (8, 'Dom', 'Female', 45, 'Headich');
 
 
 -- for Table Services
-INSERT INTO Services VALUES (1, 'Dr. Osim', '013325', 'Doctor', '36/37 BIT, KUET', 'Taligati', 'Khulna');
-INSERT INTO Services VALUES (2, 'Dr. Anton', '01425', 'Doctor', '334 k.b road', 'Saver', 'Dhaka');
-INSERT INTO Services VALUES (3, 'Dr. Raisa', '01525', 'Doctor', '45/37 Amlapara', 'Narayangonj', 'Dhaka');
-INSERT INTO Services VALUES (4, 'Dr. Otaum', '01625', 'Doctor', '556 gulshan', 'Gulshan', 'Dhaka');
+INSERT INTO Services VALUES (1001, 'Dr. Osim', '013325', 'Doctor', '36/37 BIT, KUET', 'Taligati', 'Khulna');
+INSERT INTO Services VALUES (1002, 'Dr. Anton', '01425', 'Doctor', '334 k.b road', 'Saver', 'Dhaka');
+INSERT INTO Services VALUES (1003, 'Dr. Raisa', '01525', 'Doctor', '45/37 Amlapara', 'Narayangonj', 'Dhaka');
+INSERT INTO Services VALUES (1004, 'Dr. Otaum', '01625', 'Doctor', '556 gulshan', 'Gulshan', 'Dhaka');
 
-INSERT INTO Services VALUES (5, 'Sohuug', '0132325', 'Ambulance', '36/37 BIT, KUET', 'Bagerhat', 'Khulna');
-INSERT INTO Services VALUES (6, 'Harun', '014251', 'Ambulance', '334 k.b road', 'Saver', 'Dhaka');
-INSERT INTO Services VALUES (7, 'Karim', '015254', 'Ambulance', '45/37 Amlapara', 'Narail', 'Khulna');
-INSERT INTO Services VALUES (8, 'Raton', '016257', 'Ambulance', '556 gulshan', 'Manikgonj', 'Dhaka');
+INSERT INTO Services VALUES (2001, 'Sohug', '0132325', 'Ambulance', '36/37 BIT, KUET', 'Bagerhat', 'Khulna');
+INSERT INTO Services VALUES (2002, 'Harun', '014251', 'Ambulance', '334 k.b road', 'Saver', 'Dhaka');
+INSERT INTO Services VALUES (2003, 'Karim', '015254', 'Ambulance', '45/37 Amlapara', 'Narail', 'Khulna');
+INSERT INTO Services VALUES (2004, 'Raton', '016257', 'Ambulance', '556 gulshan', 'Manikgonj', 'Dhaka');
 
-INSERT INTO Services VALUES (9, 'Medicine Plue', '0614251', 'Pharmacy', '334 k.b road', 'Taligati', 'Khulna');
-INSERT INTO Services VALUES (10, 'Larze Farma', '0152754', 'Pharmacy', '45/37 Amlapara', 'Narail', 'Khulna');
-INSERT INTO Services VALUES (11, 'Popular Medicine', '9016257', 'Pharmacy', '556 gulshan','Bagerhat', 'Khulna');
+INSERT INTO Services VALUES (3001, 'Medicine Plue', '0614251', 'Pharmacy', '334 k.b road', 'Taligati', 'Khulna');
+INSERT INTO Services VALUES (3002, 'Larze Farma', '0152754', 'Pharmacy', '45/37 Amlapara', 'Narail', 'Khulna');
+INSERT INTO Services VALUES (3003, 'Popular Medicine', '9016257', 'Pharmacy', '556 gulshan','Bagerhat', 'Khulna');
 
-INSERT INTO Services VALUES (12, 'Abu Nasir', '01527544', 'Hospital', '45/37 Amlapara', 'Taligati', 'Khulna');
-INSERT INTO Services VALUES (13, 'PG Hospital', '90162657', 'Hospital', '556 gulshan', 'Gulshan', 'Dhaka');
+INSERT INTO Services VALUES (4001, 'Abu Nasir', '01527544', 'Hospital', '45/37 Amlapara', 'Taligati', 'Khulna');
+INSERT INTO Services VALUES (4002, 'PG Hospital', '90162657', 'Hospital', '556 gulshan', 'Gulshan', 'Dhaka');
 
-INSERT INTO Services VALUES (14, 'Blood Solution', '01528754', 'Blood_Bank', '45/37 Amlapara', 'Manikgonj', 'Dhaka');
-INSERT INTO Services VALUES (15, 'Life', '90176257', 'Blood_Bank', '556 gulshan', 'Narail', 'Khulna');
+INSERT INTO Services VALUES (5001, 'Blood Solution', '01528754', 'Blood_Bank', '45/37 Amlapara', 'Manikgonj', 'Dhaka');
+INSERT INTO Services VALUES (5002, 'Life', '90176257', 'Blood_Bank', '556 gulshan', 'Narail', 'Khulna');
+
+
+
+-- for Table Check_Up
+insert into Check_up values (1, 2, 1003, to_date('22-Jun-22', 'dd-mon-yy'));
+insert into Check_up values (2, 7, 1002, to_date('19-May-22 12:00 A.M.' , 'dd-mon-yy hh:mi A.M.'));
+insert into Check_up values (3, 1, 1004, to_date('08-Apr-22 12:00 A.M.' , 'dd-mon-yy hh:mi A.M.'));
+insert into Check_up values (4, 3, 1001, to_date('08-Dec-22 12:00 A.M.' , 'dd-mon-yy hh:mi A.M.'));
+insert into Check_up values (5, 7, 1002, to_date('08-Jan-22 12:00 A.M.' , 'dd-mon-yy hh:mi A.M.'));
+
+-- Just a testing quuery 
+-- SELECT u.id,  c.name as client, s.name as doctor from check_up u,  Client c, Services s where c.id = u.client_id AND s.id= u.doctor_id;
+
+
+-- for Table Ambulance_Book
+insert into Ambulance_Book values (1, 5, 2003, '34/44 kb road, Khulna', 'Abu Nasir');
+insert into Ambulance_Book values (2, 2, 2002, 'PG hospital', '44 road, Dhanmondi');
+insert into Ambulance_Book values (3, 7, 2001, 'Khulna Medical', '56/33 Dakbangla');
+insert into Ambulance_Book values (4, 1, 2001, '34/4 kk road, Gulashan', 'Solimolla');
+insert into Ambulance_Book values (5, 3, 2002, 'Khulna Medical', 'PG Hospital');
+
+
+-- for Table Medicine_Order
+insert into Medicine_Order values (1, 2, 3003, 'Napa', 10);
+insert into Medicine_Order values (2, 7, 3002, 'Gendrin', 5);
+insert into Medicine_Order values (3, 1, 3003, 'Flazin', 8);
+insert into Medicine_Order values (4, 3, 3001, 'Amodis', 7);
+insert into Medicine_Order values (5, 7, 3002, 'Aladiba', 2);
+
+
+-- for Table Hospital_Book
+insert into Hospital_Book values (1, 1, 4001, to_date('22-Jun-22', 'dd-mon-yy'), to_date('26-Jun-22', 'dd-mon-yy'));
+insert into Hospital_Book values (2, 3, 4002, to_date('02-May-22', 'dd-mon-yy'), to_date('03-May-22', 'dd-mon-yy'));
+insert into Hospital_Book values (3, 8, 4002, to_date('07-Dec-22', 'dd-mon-yy'), to_date('12-Dec-22', 'dd-mon-yy'));
+insert into Hospital_Book values (4, 6, 4001, to_date('20-Mar-22', 'dd-mon-yy'), to_date('27-Mar-22', 'dd-mon-yy'));
+insert into Hospital_Book values (5, 4, 4002, to_date('19-Apr-22', 'dd-mon-yy'), to_date('02-May-22', 'dd-mon-yy'));
 
 
 -- for Table Blood_Bank_Info
-INSERT INTO Blood_Bank_Info VALUES(14, 20, 5, 0, 7, 58, 9, 23, NULL);
-INSERT INTO Blood_Bank_Info VALUES(15, NULL, 5, 10, 4, 8, 7, 2, 6);
+INSERT INTO Blood_Bank_Info VALUES(5001, 20, 5, 0, 7, 58, 9, 23, NULL);
+INSERT INTO Blood_Bank_Info VALUES(5002, NULL, 5, 10, 4, 8, 7, 2, 6);
