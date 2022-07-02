4c06dba72b6185e1a01704260290cc62ae9c626c
AnkanSaha18 <95054004+AnkanSaha18@users.noreply.github.com>
2022-07-02 13:14:25 +0600

    New Values added

--- Project file/Values.sql
+++ Project file/Values.sql
@@ -18,13 +18,90 @@ INSERT INTO District VALUES ('Manikgonj', 'Dhaka');
 INSERT INTO District VALUES ('Saver', 'Dhaka');
 INSERT INTO District VALUES ('Tangail', 'Dhaka');
 INSERT INTO District VALUES ('Gazipur', 'Dhaka');
+insert into District values ('Krasnolesnyy', 'Dhaka');
+insert into District values ('Phong Điền', 'Dhaka');
+insert into District values ('Kabinda', 'Dhaka');
+insert into District values ('Miandrarivo', 'Dhaka');
+insert into District values ('Wangcheng', 'Dhaka');
+insert into District values ('San Andres', 'Dhaka');
+insert into District values ('Obsza', 'Dhaka');
+
+insert into District values ('Pajagan', 'Chittagong');
+insert into District values ('Oyem', 'Chittagong');
+insert into District values ('Huizhai', 'Chittagong');
+insert into District values ('Övertorneå', 'Chittagong');
+insert into District values ('Pilcomay', 'Chittagong');
+insert into District values ('Ehen Hudag', 'Chittagong');
+insert into District values ('Salimbalan', 'Chittagong');
+insert into District values ('Jargalant', 'Chittagong');
+insert into District values ('Mobile', 'Chittagong');
+
+insert into District values ('Dongzhaogezhuang', 'Sylhet');
+insert into District values ('Carlton', 'Sylhet');
+insert into District values ('Gzy', 'Sylhet');
+insert into District values ('Dalang', 'Sylhet');
+insert into District values ('Guoyuan', 'Sylhet');
+insert into District values ('Urek-urek', 'Sylhet');
+insert into District values ('Huilongshan', 'Sylhet');
+insert into District values ('Meilin', 'Sylhet');
+insert into District values ('Sedan', 'Sylhet');
+insert into District values ('Brétigny-sur-Orge', 'Sylhet');
+insert into District values ('Oslo', 'Sylhet');
+
+insert into District values ('Sztum', 'Barisal');
+insert into District values ('Tuzha', 'Barisal');
+insert into District values ('Ash Shafā', 'Barisal');
+insert into District values ('Pilar', 'Barisal');
+insert into District values ('Trinidad', 'Barisal');
+insert into District values ('San Juan', 'Barisal');
+insert into District values ('Lagkadás', 'Barisal');
+insert into District values ('La Guadalupe', 'Barisal');
+insert into District values ('Plaisir', 'Barisal');
+insert into District values ('Hengshan', 'Barisal');
+insert into District values ('Dawang', 'Barisal');
+
+insert into District values ('Thamaga', 'Rajshahi');
+insert into District values ('Dongkengkou', 'Rajshahi');
+insert into District values ('Traganón', 'Rajshahi');
+insert into District values ('Gniechowice', 'Rajshahi');
+insert into District values ('Omišalj', 'Rajshahi');
+insert into District values ('Jomda', 'Rajshahi');
+insert into District values ('Zhob', 'Rajshahi');
+insert into District values ('Kousséri', 'Rajshahi');
+insert into District values ('Konary', 'Rajshahi');
 
 INSERT INTO District VALUES ('Bagerhat', 'Khulna');
 INSERT INTO District VALUES ('Jashore', 'Khulna');
 INSERT INTO District VALUES ('Kustia', 'Khulna');
 INSERT INTO District VALUES ('Narail', 'Khulna');
 INSERT INTO District VALUES ('Taligati', 'Khulna');
+insert into District values ('Chaiyaphum', 'Khulna');
+insert into District values ('Corupá', 'Khulna');
+insert into District values ('Morelos', 'Khulna');
+insert into District values ('Jishan', 'Khulna');
+insert into District values ('Changfu', 'Khulna');
+insert into District values ('Dinghuo', 'Khulna');
+insert into District values ('Fulu', 'Khulna');
 
+insert into District values ('Wulan', 'Rangpur');
+insert into District values ('Ḩakamā', 'Rangpur');
+insert into District values ('Chita', 'Rangpur');
+insert into District values ('Pasadena', 'Rangpur');
+insert into District values ('Siwa Oasis', 'Rangpur');
+insert into District values ('Budzów', 'Rangpur');
+insert into District values ('Batsari', 'Rangpur');
+insert into District values ('Mazowe', 'Rangpur');
+insert into District values ('Zhangtan', 'Rangpur');
+insert into District values ('Ubaitaba', 'Rangpur');
+
+insert into District values ('El Zapote', 'Mymensingh');
+insert into District values ('Dārān', 'Mymensingh');
+insert into District values ('Göteborg', 'Mymensingh');
+insert into District values ('København', 'Mymensingh');
+insert into District values ('Lalagsan', 'Mymensingh');
+insert into District values ('Shatoujiao', 'Mymensingh');
+insert into District values ('Akonolinga', 'Mymensingh');
+insert into District values ('Ransang', 'Mymensingh');
 
 -- for Table Client
 INSERT INTO Client VALUES (1, 'Alex', 'Male', 45, null);
@@ -35,13 +112,42 @@ INSERT INTO Client VALUES (5, 'Horton', 'Feale', 45, 'Teeth Pain');
 INSERT INTO Client VALUES (6, 'Any', 'Female', 45, 'Back Pain');
 INSERT INTO Client VALUES (7, 'Solax', 'Male', 45, null);
 INSERT INTO Client VALUES (8, 'Dom', 'Female', 45, 'Headich');
+insert into Client values (9, 'Sheffy', 'Male', 94, '');
+insert into Client values (10, 'Julee', 'Female', 70, 'Suspendisse potenti.');
+insert into Client values (11, 'Jennine', 'Female', 34, '');
+insert into Client values (12, 'Allister', 'Male', 86, '');
+insert into Client values (13, 'Lawton', 'Male', 90, '');
+insert into Client values (14, 'Ingram', 'Male', 79, '');
+insert into Client values (15, 'Elena', 'Female', 63, 'Vivamus vel nullapellentesque.');
+insert into Client values (16, 'Tawnya', 'Female', 9, '');
+insert into Client values (17, 'David', 'Male', 62, 'Quisque porta volutpat erat.');
+insert into Client values (18, 'Elli', 'Female', 8, '');
+insert into Client values (19, 'Clayson', 'Male', 82, '');
+insert into Client values (20, 'Akim', 'Male', 96, '');
+insert into Client values (21, 'Jeanna', 'Female', 40, 'Maecenas tristique, est et tempus semper.');
+insert into Client values (22, 'Marthena', 'Agender', 41, '');
+insert into Client values (23, 'Todd', 'Male', 39, 'Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.');
+insert into Client values (24, 'Shaylah', 'Female', 10, 'Sed accumsan felis.');
+insert into Client values (25, 'Madlen', 'Female', 45, 'Etiam faucibus cursus urna.');
+insert into Client values (26, 'Clio', 'Female', 89, 'Nam dui.');
+insert into Client values (27, 'Julianna', 'Female', 74, '');
+insert into Client values (28, 'Calypso', 'Female', 11, '');
+insert into Client values (29, 'Laurene', 'Female', 37, 'Pellentesque ultrices mattis odio.');
+insert into Client values (30, 'Chryste', 'Female', 95, 'Duis bibendum, felis sed interdum venenatis.');
+insert into Client values (31, 'Merill', 'Male', 100, '');
+insert into Client values (32, 'Berenice', 'Female', 59, '');
+insert into Client values (33, 'Onfroi', 'Male', 93, '');
 
 
 -- for Table Services
 INSERT INTO Services VALUES (1001, 'Dr. Osim', '013325', 'Doctor', '36/37 BIT, KUET', 'Taligati', 'Khulna');
 INSERT INTO Services VALUES (1002, 'Dr. Anton', '01425', 'Doctor', '334 k.b road', 'Saver', 'Dhaka');
-INSERT INTO Services VALUES (1003, 'Dr. Raisa', '01525', 'Doctor', '45/37 Amlapara', 'Narayangonj', 'Dhaka');
-INSERT INTO Services VALUES (1004, 'Dr. Otaum', '01625', 'Doctor', '556 gulshan', 'Gulshan', 'Dhaka');
+INSERT INTO Services VALUES (1003, 'Dr. Raisa', '01525', 'Doctor', '45/37 Amlapara','Ransang', 'Mymensingh');
+INSERT INTO Services VALUES (1004, 'Dr. Otaum', '01625', 'Doctor', '556 gulshan', 'Krasnolesnyy', 'Dhaka');
+INSERT INTO Services VALUES (1005, 'Dr. Elli', '013234325', 'Doctor', '36/37 BIT, KUET', 'Taligati', 'Khulna');
+INSERT INTO Services VALUES (1006, 'Dr. Clayson', '0145425', 'Doctor', '334 k.b road', 'Meilin', 'Sylhet');
+INSERT INTO Services VALUES (1007, 'Dr. Akim', '0152545', 'Doctor', '45/37 Amlapara', 'Narayangonj', 'Dhaka');
+INSERT INTO Services VALUES (1008, 'Dr. Jeanna', '01345625', 'Doctor', '556 gulshan', 'Ransang', 'Mymensingh');
 
 INSERT INTO Services VALUES (2001, 'Sohug', '0132325', 'Ambulance', '36/37 BIT, KUET', 'Bagerhat', 'Khulna');
 INSERT INTO Services VALUES (2002, 'Harun', '014251', 'Ambulance', '334 k.b road', 'Saver', 'Dhaka');
@@ -98,3 +204,5 @@ insert into Hospital_Book values (5, 4, 4002, to_date('19-Apr-22', 'dd-mon-yy'),
 -- for Table Blood_Bank_Info
 INSERT INTO Blood_Bank_Info VALUES(5001, 20, 5, 0, 7, 58, 9, 23, NULL);
 INSERT INTO Blood_Bank_Info VALUES(5002, NULL, 5, 10, 4, 8, 7, 2, 6);
+
+
