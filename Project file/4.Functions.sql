3ee2e30dfc3ba326aff19c461a7138e67c4ca254
AnkanSaha18 <95054004+AnkanSaha18@users.noreply.github.com>
2022-07-17 11:16:52 +0600

    if elsif added

--- Project file/4.Functions.sql
+++ Project file/4.Functions.sql
@@ -5,6 +5,21 @@ CURSOR div IS SELECT * FROM Division;
 divName div%ROWTYPE;
 
 BEGIN
+if type = 'Doctor' then
+    DBMS_OUTPUT.PUT_LINE('Called For Doctor');
+elsif type = 'Ambulance' then
+    DBMS_OUTPUT.PUT_LINE('Called For Ambulance');
+elsif type = 'Pharmacy' then
+    DBMS_OUTPUT.PUT_LINE('Called For Pharmacy');
+elsif type = 'Hospital' then
+    DBMS_OUTPUT.PUT_LINE('Called For Hospital');
+elsif type = 'Blood_Bank' then
+    DBMS_OUTPUT.PUT_LINE('Called For Blood Bank');
+END if;
+
+
+
+
 DBMS_OUTPUT.PUT_LINE(type || ' Services Area are: ');
 OPEN div;
     while div%ROWCOUNT <=7
